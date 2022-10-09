b3ef84572ee9dbc5f051566a9c5d1dc34b95509d
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-09 14:21:01 +0200

    Start of diversity analysis

---
+++ src/obofoundry/diversity_analysis.py
@@ -0,0 +1,199 @@
+"""Diversity analysis will focus on country/continent and gender."""
+import json
+from collections import defaultdict
+from textwrap import dedent
+
+import click
+import pystow
+import requests
+import yaml
+from tqdm import tqdm
+
+from obofoundry.constants import ALUMNI_METADATA_PATH, OPERATIONS_METADATA_PATH
+from obofoundry.utils import (
+    get_data,
+    get_github,
+    get_github_user,
+    get_repositories,
+    query_wikidata,
+)
+
+SERVICE = (
+    'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }'
+)
+SELECT_CLAUSE = dedent(
+    """\
+    ?person ?personLabel 
+    ?orcid 
+    ?github 
+    ?genderLabel
+    ?citizenship
+    ?nativelanguage
+    """
+)
+QUERY_BITS = dedent(
+    """\
+    OPTIONAL { ?person wdt:P21 ?gender }
+    OPTIONAL { ?person wdt:P27 ?citizenship }
+    OPTIONAL { ?person wdt:P103 ?nativelanguage }
+    OPTIONAL { ?person wdt:P2037 ?github }
+    """
+)
+
+
+def get_responsible_wikidatas():
+    orcids = {
+        orcid
+        for d in get_data().values()
+        if (orcid := d.get("contact", {}).get("orcid"))
+    }
+    x = " ".join(f'"{orcid}"' for orcid in orcids)
+    sparql = dedent(
+        f"""\
+        SELECT DISTINCT {SELECT_CLAUSE}
+        WHERE
+        {{
+            VALUES ?orcid {{ {x} }}
+            ?person wdt:P496 ?orcid
+            {QUERY_BITS}
+            {SERVICE}
+        }}
+        """
+    )
+    res = query_wikidata(sparql)
+    return {
+        record["person"]["value"].removeprefix("http://www.wikidata.org/entity/")
+        for record in res
+    }
+
+
+def get_wikidata_ids():
+    rv = set()
+    for path in [OPERATIONS_METADATA_PATH, ALUMNI_METADATA_PATH]:
+        rv.update(
+            member["wikidata"] for member in yaml.safe_load(path.read_text())["members"]
+        )
+    click.echo("querying responsible authors in wikidata")
+    rv.update(get_responsible_wikidatas())
+    click.echo("done")
+    return rv
+
+
+def main():
+    contributors = {}
+    author_to_prefix = defaultdict(set)
+    for prefix, (owner, name) in tqdm(
+        sorted(get_repositories().items()), unit="ontology"
+    ):
+        path = pystow.join(
+            "obofoundry", "analysis", "diversity", name=f"{prefix}_contributors.json"
+        )
+        if path.is_file():
+            prefix_contributors = json.loads(path.read_text())
+        else:
+            res_json = get_github(
+                f"https://api.github.com/repos/{owner}/{name}/contributors?per_page=100"
+            )
+            prefix_contributors = {}
+            for github_logins in res_json:
+                login = github_logins["login"]
+                github_user = get_github_user(login)
+                prefix_contributors[login] = {
+                    "contributions": github_logins["contributions"],
+                    "name": github_user.get("name"),
+                    "email": github_user.get("email"),
+                    "bio": github_user.get("bio"),
+                    "blog": github_user.get("blog"),
+                    "company": github_user.get("company"),
+                    "twitter_username": github_user.get("twitter_username"),
+                }
+            path.write_text(json.dumps(prefix_contributors, indent=2, sort_keys=True))
+        for login in prefix_contributors:
+            author_to_prefix[login].add(prefix)
+        contributors.update(prefix_contributors)
+
+    #: include accounts that are not for people or ones that will never be resolvable
+    login_blacklist = {
+        "github-actions[bot]",
+        "dependabot[bot]",
+        "uberon",
+        "rsc-ontologies",
+        "txpo-ontology",
+        "actions-user",
+        "ontobot",
+        "GoogleCodeExporter",
+        "labda",
+        "bbopjenkins",
+        "echinoderm-ontology",
+        "OnToologyUser",
+        "scdodev",
+        # Not enough information/obscured name
+        "wmbio",
+        "WMBio",
+        "Antonarctica",
+        "Jguzman210",
+        "ccadete",
+        # Might be possible with more work
+        "DiegoRegalado",
+        # Not an academic
+        "mfjackson",
+        ##############################################
+        # Almost got the following, will leave notes #
+        ##############################################
+        "CMCosta",
+        # contributed to nmrML, probably Christopher Costa (author of
+        # https://www.sciencedirect.com/science/article/pii/S0169260715300535?via%3Dihub#!)
+        "Huffmaar",  # anythony huffman, graduate student of Oliver He
+    }
+    it = {
+        (
+            f'"{login}"',
+            "UNDEF"
+            if data["name"] is None
+            else '"' + data["name"].replace('"', "") + '"',
+            '"' + ", ".join(sorted(author_to_prefix[login])) + '"',
+        )
+        for login, data in contributors.items()
+        if login.lower() not in login_blacklist and login not in login_blacklist
+    }
+    github_logins = " ".join("(" + " ".join(t) + ")" for t in sorted(it))
+    sparql = f"""\
+SELECT DISTINCT ?github ?name ?ontologies ?person ?personLabel ?genderLabel
+WHERE
+{{
+    VALUES (?github ?name ?ontologies) {{ 
+        {github_logins} 
+    }}
+    OPTIONAL {{
+        ?person wdt:P2037 ?github .
+        OPTIONAL {{ ?person wdt:P496 ?orcid }}
+        OPTIONAL {{ ?person wdt:P21 ?gender }}
+    }}
+    {SERVICE}
+}}
+ORDER BY ?person DESC(?name)
+"""
+    import pyperclip
+
+    pyperclip.copy(sparql)
+
+    return
+    wikidata_ids = get_wikidata_ids()
+    github_logins = " ".join(f"wd:{v}" for v in sorted(wikidata_ids))
+    sparql = dedent(
+        f"""\
+        SELECT DISTINCT {SELECT_CLAUSE}
+        WHERE
+        {{
+            VALUES ?person {{ {github_logins} }}
+            OPTIONAL {{ ?person wdt:P2037 ?orcid }}
+            {QUERY_BITS}
+            {SERVICE}
+        }}
+        """
+    )
+    print(sparql)
+
+
+if __name__ == "__main__":
+    main()
--- src/obofoundry/utils.py
+++ src/obofoundry/utils.py
@@ -1,7 +1,13 @@
 """Test data integrity, beyond what's possible with the JSON schema."""
 
+import json
+from textwrap import dedent
+from typing import Optional
+
+import pystow
 import requests
 import yaml
+from ratelimit import rate_limited
 
 from obofoundry.constants import ONTOLOGY_DIRECTORY
 
@@ -28,16 +34,100 @@ def get_data():
     return ontologies
 
 
+def get_repositories():
+    """Get a mapping to GitHub repositories."""
+    rv = {}
+    for key, data in get_data().items():
+        repository = data.get("repository") or ""
+        if "github" not in repository:
+            continue
+        owner, repo = (
+            repository.removeprefix("https://github.com/").rstrip("/").split("/")
+        )
+        rv[key] = (owner, repo)
+    return rv
+
+
 #: WikiData SPARQL endpoint. See https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service#Interfacing
-WIKIDATA_SPARQL = "https://query.wikidata.org/bigdata/namespace/wdq/sparql"
+WIKIDATA_SPARQL = "https://query.wikidata.org/sparql"
 
 
 def query_wikidata(query: str):
     """Query the Wikidata SPARQL endpoint and return JSON."""
-    headers = {"User-Agent": "obofoundry/1.0 (https://obofoundry.org)"}
+    headers = {
+        "User-Agent": "obofoundry/1.0 (https://obofoundry.org)",
+        "Accept": "application/sparql-results+json",
+    }
     res = requests.get(
         WIKIDATA_SPARQL, params={"query": query, "format": "json"}, headers=headers
     )
     res.raise_for_status()
     res_json = res.json()
     return res_json["results"]["bindings"]
+
+
+@rate_limited(calls=5_000, period=60 * 60)
+def get_github(
+    url: str,
+    accept: Optional[str] = None,
+    params: Optional[dict[str, any]] = None,
+    token: Optional[str] = None,
+):
+    """Query the GitHub API."""
+    # Load the GitHub access token via PyStow. We'll
+    # need it so we don't hit the rate limit
+    token = pystow.get_config(
+        "github", "token", raise_on_missing=True, passthrough=token
+    )
+    headers = {
+        "Authorization": f"token {token}",
+    }
+    if accept:
+        headers["Accept"] = accept
+    return requests.get(url, headers=headers, params=params).json()
+
+
+def get_github_user(u, **kwargs):
+    """Get GitHub user data."""
+    path = pystow.join("github", "user", name=f"{u.lower()}.json")
+    if path.is_file():
+        return json.loads(path.read_text())
+    url = f"https://api.github.com/users/{u}"
+    res = get_github(url, **kwargs)
+    path.write_text(json.dumps(res, indent=2, sort_keys=True))
+    return res
+
+
+def _():
+    z = ",\n".join(
+        f"""  {prefix}: repository(owner: "{owner}", name: "{name}") {{...repoProperties}}"""
+        for prefix, (owner, name) in sorted(get_repositories().items())
+    )
+
+    s = (
+        dedent(
+            """
+    fragment repoProperties on Repository {
+      id
+      name
+      collaborators(first: 10, affiliation: ALL) {
+        edges {
+          node {
+            login
+            name
+          }
+        }
+      }
+    }
+    query {
+      %s
+    }
+    """
+        )
+        % z
+    )
+    print(s)
+
+
+if __name__ == "__main__":
+    _()
